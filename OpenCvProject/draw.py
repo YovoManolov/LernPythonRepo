67747d445de7f7a2beaba88e9a76596fb596c161
yovo <yovo.manolov@abv.bg>
2021-06-09 22:44:30 +0300

    commit python opencv first files and code

---
+++ OpenCvProject/draw.py
@@ -0,0 +1,34 @@
+import cv2 as cv
+import numpy as np
+
+blank = np.zeros((500, 500, 3), dtype='uint8')
+#cv.imshow('Blank', blank)
+
+# 1. Paint the image certain colour
+
+# blank[200:300, 300:400] = 0, 0, 255
+# cv.imshow('Red', blank)
+
+# # 2. Draw a rectangle
+# cv.rectangle(blank, (0, 0),
+#              (blank.shape[1]//2, blank.shape[0]//2), (0, 255, 0), thickness=cv.FILLED)
+# cv.imshow('Rectangle', blank)
+
+
+# # 3. Drow A Circle
+# cv.circle(blank, (blank.shape[1]//2,
+#           blank.shape[0]//2), 40, (0, 0, 255), thickness=-1)
+# cv.imshow('Circle', blank)
+
+# # Draw a Line
+# cv.line(blank, (100, 250), (300, 400), (255, 255, 255), thickness=3)
+
+# cv.imshow('Line', blank)
+# cv.waitKey(0)
+
+
+# 5. Write a text
+cv.putText(blank, 'Hello, my name is Json!!!', (10, 255),
+           cv.FONT_HERSHEY_TRIPLEX, 1.0, (0, 255, 0), 2)
+cv.imshow('Text', blank)
+cv.waitKey(0)
